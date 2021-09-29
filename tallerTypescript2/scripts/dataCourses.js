99b531ef75816d28b75e9654c9bef619b0da9cdb
Juan Alegría <j.alegria@uniandes.edu.co>
2021-09-29 08:40:31 -0500

    Build: make TS1 starting point of TS2

---
+++ tallerTypescript2/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW", "César Forero", 4),
+    new Course("Probabilidad y Estadística", "Nicolás Mejía", 2),
+    new Course("Algoritmos", "Carlos Fuentes", 2),
+    new Course("Estructuras de Datos", "Yesid D", 1),
+    new Course("Juegos, Redes y Complejidad", "Ana Guerra", 6)
+];
---
+++ tallerTypescript2/scripts/main.js
@@ -0,0 +1,40 @@
+import { dataCourses } from './dataCourses.js';
+var coursesTbody = document.getElementById('courses');
+var btnfilterByName = document.getElementById("button-filterByName");
+var inputSearchBox = document.getElementById("search-box");
+var totalCreditElm = document.getElementById("total-credits");
+btnfilterByName.onclick = function () { return applyFilterByName(); };
+renderCoursesInTable(dataCourses);
+totalCreditElm.innerHTML = "" + getTotalCredits(dataCourses);
+function renderCoursesInTable(courses) {
+    console.log('Desplegando cursos');
+    courses.forEach(function (course) {
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>" + course.name + "</td>\n                           <td>" + course.professor + "</td>\n                           <td>" + course.credits + "</td>";
+        coursesTbody.appendChild(trElement);
+    });
+}
+function applyFilterByName() {
+    var text = inputSearchBox.value;
+    text = (text == null) ? '' : text;
+    clearCoursesInTable();
+    var coursesFiltered = searchCourseByName(text, dataCourses);
+    renderCoursesInTable(coursesFiltered);
+}
+function searchCourseByName(nameKey, courses) {
+    return nameKey === '' ? dataCourses : courses.filter(function (c) {
+        return c.name.match(nameKey);
+    });
+}
+function getTotalCredits(courses) {
+    var totalCredits = 0;
+    courses.forEach(function (course) { return totalCredits = totalCredits + course.credits; });
+    return totalCredits;
+}
+function clearCoursesInTable() {
+    while (coursesTbody.hasChildNodes()) {
+        if (coursesTbody.firstChild != null) {
+            coursesTbody.removeChild(coursesTbody.firstChild);
+        }
+    }
+}
